85f32aa097d830142c03cc64da00b2b66e4a7f86
Proforty7 <proforty7@gmail.com>
2018-12-20 18:59:39 +0530

    Changed topic
    Signed-off-by: Proforty7 <proforty7@gmail.com>

--- routes/topics.js
+++ routes/topics.js
@@ -9,11 +9,11 @@ route.get("/cpp", (req, res) => {
   });
 });
 
-route.post("/cpp/add-topic", (req, res) => {
+route.post("/add-topic", (req, res) => {
   const name = req.body.name;
   const url = req.body.url;
   const topic = new Topic({
-    type: "cpp",
+    type: req.params.type,
     name: name,
     url: url
   });
